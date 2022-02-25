86c9fc2f2b78f5426ef8bf8a5e066f1aa9aa5203
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-02-26 07:58:11 +0800

    template update

--- template.cpp
+++ template.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 #define MOD 10e9+7
-using namespace std;
+typedef long double ld;
 typedef long long ll;
+using namespace std;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); //fast I/O
