748bde371a4d286ea996e675dd993276fd91a2b1
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-02-26 08:07:09 +0800

    add advanced template

---
+++ advTemplate.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define MOD 10e9+7
+#define pb push_back
+#define vi vector<int>
+#define vii vector<int, int>
+typedef long double ld;
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); //fast I/O
+    
+    return 0;
+}
