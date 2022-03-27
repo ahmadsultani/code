28d81a61f16b23d451222f7c7488f695897c49ec
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-03-27 21:07:11 +0800

    leetcode problem

---
+++ LeetCode/duplicateZeros.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void duplicateZero(vector<int> &arr){
+        //O()
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); //fast I/O    
+    vector<int> arr = {0,1,2,3,4,5,6};
+
+    cout << "\n";
+    return 0;
+}
