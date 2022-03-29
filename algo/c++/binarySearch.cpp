00e25005bc8e423348fc82573101f8c00dbdf16f
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-03-30 07:52:28 +0800

    update on binary search

--- algo/c++/binarySearch.cpp
+++ algo/c++/binarySearch.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int* arr, int target, int len){
+int binarySearch(int arr[], int target, int len){
     int left = 0, right = len - 1;
     while(left <= right) {
-        int mid = (left + right) / 2;
+        int mid = left + (right-left) / 2;
         if (arr[mid] == target) return mid;
         else if (arr[mid] < target) left = mid + 1;
         else right = mid - 1; 
     }
-    return -1;
+    return -1; //target not found
 }
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0); //fast I/O
     int arr[] = {1, 2, 3, 5, 7, 8, 9, 10, 18, 19, 21, 20};
     int target = 9;
     cout << "target found at index ";
