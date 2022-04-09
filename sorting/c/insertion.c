ffcb861e6547c03c32be05b3dbb73b2f4d65bfe4
dayanullahas21d@student.unhas.ac.id <dayanullahas21d@student.unhas.ac.id>
2022-04-09 09:32:18 +0800

    insertion sort

--- sorting/c/insertion.c
+++ sorting/c/insertion.c
@@ -4,7 +4,7 @@ void swap(int*, int*);
 void insertionSort(int*, int);
 
 int main() {
-    int arr[] = {11,21,34,40,15,13,7,9,10,8};
+    int arr[] = {11,21,34,40,15,13,9,10,8};
     insertionSort(arr, sizeof(arr)/sizeof(arr[0]));
     for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
         printf("%d ", arr[i]);
@@ -12,17 +12,14 @@ int main() {
     return 0;
 }
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void insertionSort(int* arr, int size){
-    int key = arr[0];
-    for(i = 0; i < size; i++){
-        for(j = 0; j < size - i; j++){
-            if(arr[j] > arr[j + 1]) swap(&arr[j], &arr[j + 1]);
+void insertionSort(int* arr, int size) {
+    for (int step = 1; step < size; step++) {
+        int key = arr[step];
+        int j = step - 1;
+        while (key < arr[j] && j >= 0) {
+            arr[j + 1] = arr[j];
+            --j;
         }
+        arr[j + 1] = key;
     }
 }
